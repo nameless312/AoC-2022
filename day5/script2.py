f20350be30749ac9800a5c9533f482612324f0fb
Nameless <jokinha.2001@gmail.com>
2022-12-05 23:08:39 +0000

    day5

---
+++ day5/script2.py
@@ -0,0 +1,39 @@
+def read_input():
+	with open("input.txt") as fp:
+		lines = fp.readlines()
+		row = list(lines[0])
+		size = len(row) // 4
+		matrix = [[] for _ in range(size)]
+		ops = []
+		for line in lines:
+			if not line.startswith('\n') and not line.startswith('move') and not line.startswith(' 1'):
+				row = list(line)
+				i = 1
+				for j in range(size):
+					if row[i] != ' ':
+						matrix[j].insert(0,row[i])
+					i += 4
+			elif line.startswith('move'):
+				op = []
+				line = line.strip().split(" ")
+				ops.append((int(line[1]),int(line[3]),int(line[5])))
+		return matrix, ops
+
+def iterate_ops(matrix,ops):
+
+	for (q, pos1, pos2) in ops:
+		temp = []
+		for i in range(q):
+			el = matrix[pos1-1].pop()
+			temp.insert(0,el)
+		for el in temp:
+			matrix[pos2-1].append(el)
+	return "".join([x[len(x) - 1] for x in matrix])
+
+def solve():
+	matrix, ops = read_input()
+	res = iterate_ops(matrix, ops)
+	print(res)
+
+if __name__ == "__main__":
+	solve()
